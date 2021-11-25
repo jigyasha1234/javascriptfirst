be8e4dfdd1d23f745145c4c363d348c71c11e780
Jigyasha Rani <jigrani0000@gmail.com>
2021-11-25 11:21:32 +0530

    javascript-sol1

--- assignments/bmi_calculator/bmi_calculator.js
+++ assignments/bmi_calculator/bmi_calculator.js
@@ -1,6 +1,19 @@
 // This function should return the BMI for a person 
 function BMICalculator(mass, height) {
     // Write your code here
+    if(height<=0 || mass<=0 || (height<=0 && mass<=0)){
+        return("INVALID INPUT");
+    }
+    else{
+    var BMI;
+    BMI = mass / (height * height);
+    if(BMI<=0){
+        return("INVALID INPUT");
+    }
+    else{
+    return(BMI);
+    }    
+}
 }
 
 module.exports = BMICalculator;
