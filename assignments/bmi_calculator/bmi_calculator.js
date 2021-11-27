edb662cf15ceff18ab879fa3068a63bd8d0d2fc5
Jigyasha Rani <jigrani0000@gmail.com>
2021-11-27 14:50:40 +0530

    correction

--- assignments/bmi_calculator/bmi_calculator.js
+++ assignments/bmi_calculator/bmi_calculator.js
@@ -1,7 +1,7 @@
 // This function should return the BMI for a person 
 function BMICalculator(mass, height) {
     // Write your code here
-    if(height<=0 || mass<=0 || (height<=0 && mass<=0)){
+    if(height<=0 || mass<=0){
         return("INVALID INPUT");
     }
     else{
